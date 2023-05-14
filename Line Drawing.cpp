1ff8395f2b58392eec9c6a7b34c6af489e44d66d
Vittesh Raghavan <96478231+VitteshRag@users.noreply.github.com>
2023-05-15 01:02:13 +0530

    Added Line Drawing Algorithm code

--- Line Drawing.cpp
+++ Line Drawing.cpp
@@ -1,38 +1,43 @@
-#include<stdio.h>
-#include<graphics.h>
-void drawline(int x0, int y0, int x1, int y1)
+#include <graphics.h>
+#include <stdio.h>
+#include <math.h>
+#include <dos.h>
+
+int main( )
 {
-    int dx, dy, p, x, y;
-    dx=x1-x0;
-    dy=y1-y0;
-    x=x0;
-    y=y0;
-    p=2*dy-dx;
-    while(x<x1)
+    float x,y,x1,y1,x2,y2,dx,dy,step;
+    int i,gd=DETECT,gm;
+
+    initgraph(&gd,&gm,"c:\\turboc3\\bgi");
+
+    printf("Enter the value of x1 and y1 : ");
+    scanf("%f%f",&x1,&y1);
+    printf("Enter the value of x2 and y2: ");
+    scanf("%f%f",&x2,&y2);
+
+    dx=abs(x2-x1);
+    dy=abs(y2-y1);
+
+    if(dx>=dy)
+        step=dx;
+    else
+        step=dy;
+
+    dx=dx/step;
+    dy=dy/step;
+
+    x=x1;
+    y=y1;
+
+    i=1;
+    while(i<=step)
     {
-        delay(10);
-        if(p>=0)
-        {
-            putpixel(x,y,7);
-            y=y+1;
-            p=p+2*dy-2*dx;
-        }
-        else
-        {
-            putpixel(x,y,7);
-            p=p+2*dy;}
-            x=x+1;
-        }
-}
-int main()
-{
-    int gdriver=DETECT, gmode, error, x0, y0, x1, y1;
-    initgraph(&gdriver, &gmode, "c:\\turboc3\\bgi");
-    printf("Enter co-ordinates of first point: ");
-    scanf("%d%d", &x0, &y0);
-    printf("Enter co-ordinates of second point: ");
-    scanf("%d%d", &x1, &y1);
-    drawline(x0, y0, x1, y1);
+        putpixel(x,y,5);
+        x=x+dx;
+        y=y+dy;
+        i=i+1;
+        delay(100);
+    }
     getch();
-    return 0;
+    closegraph();
 }
